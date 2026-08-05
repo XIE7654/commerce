0459b355561975231f9bb57d2ed0354522097580
xie7654 <765462425@qq.com>
2026-08-05 10:18:40 +0800

    amazon

---
+++ yudao-module-temu/src/main/java/cn/iocoder/yudao/module/temu/service/addproducts/AddProductsService.java
@@ -0,0 +1,18 @@
+package cn.iocoder.yudao.module.temu.service.addproducts;
+
+import cn.iocoder.yudao.module.temu.controller.admin.addproducts.vo.AddProductsCatsReqVO;
+import tools.jackson.databind.JsonNode;
+
+/**
+ * Add Products 商品发布相关业务 Service。
+ */
+public interface AddProductsService {
+
+    /**
+     * 查询 Temu 商品分类。
+     *
+     * @param request 查询参数，包含站点、授权 Token、语言和可选父分类 ID
+     * @return Temu 官方分类查询响应
+     */
+    JsonNode getCategories(AddProductsCatsReqVO request);
+}
